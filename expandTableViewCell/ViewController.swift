67a9b541abc1c27fbaa2e9eac0d8fa6af8076e1a
Andrei <andrei.volkau94@gmail.com>
2021-01-12 18:10:57 +0300

    Initial Commit

---
+++ expandTableViewCell/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  expandTableViewCell
+//
+//  Created by Andrei Volkau on 12.01.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
